9c2b2c9e4367a013206c582ab4e39cd5dd618bb2
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-09-18 16:18:56 -0300

    feat: criando um prototipo de modal com a biblioteca Radix UI, ao clicar no botao publicar anuncio, apareca um janela por cima da tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { GameBanner } from './components/GameBanner';
+import * as Dialog from '@radix-ui/react-dialog';
 
 import './styles/main.css';
 import logo from './assets/images/logo.svg';
@@ -41,7 +42,20 @@ function App() {
       }) }
     </div>
 
-    <CreateAdBanner />
+    <Dialog.Root>
+      <CreateAdBanner />
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
+        
+        <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Dialog.Title>Publique um anúncio</Dialog.Title>
+
+          <Dialog.Content>
+            Apareça modal!
+          </Dialog.Content>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
    </div>
   )
 }
